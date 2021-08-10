9fb4c40813645efb64c3af0215b4b02b888cc077
{bobsini601} <{psh7130@naver.com}>
2021-08-10 11:07:23 +0900

    delete the comment and amend little

--- pybo/models.py
+++ pybo/models.py
@@ -2,8 +2,8 @@ from pybo import db
 
 class TimeTable(db.Model):
     id = db.Column(db.Integer, primary_key=True)
-    # user_id = db.Column(db.Integer, db.ForeignKey('user.id', ondelete='CASCADE'))
-    # user = db.relationship('User', backref=db.backref('time_table'))
+    user_id = db.Column(db.Integer, db.ForeignKey('user.id', ondelete='CASCADE'))
+    user = db.relationship('User', backref=db.backref('time_table'))
     monClass = db.Column(db.String(200))
     tuesClass = db.Column(db.String(200))
     wedClass = db.Column(db.String(200))
@@ -15,14 +15,14 @@ class TimeTable(db.Model):
 class DrowsinessData(db.Model):
     __table_name__='DrowsinessData'
     d_data=db.Column(db.Integer,primary_key=True)
-    # user_id = db.Column(db.Integer, db.ForeignKey('user.id', ondelete='CASCADE'))
-    # user = db.relationship('User', backref=db.backref('time_table'))
+    user_id = db.Column(db.Integer, db.ForeignKey('user.id', ondelete='CASCADE'))
+    user = db.relationship('User', backref=db.backref('time_table'))
     d_time=db.Column(db.DateTime,nullable=False)
     d_picture=db.Column(db.String(100),default='Unknown.jpg')
     start_time=db.Column(db.DateTime,nullable=False)
     end_time=db.Column(db.DateTime,nulable=False)
 
-   class User(db.Model):
+class User(db.Model):
     user_id = db.Column(db.String(6), primary_key=True, nullable=False)
     nickname = db.Column(db.String(20), nullable=False)
     id = db.Column(db.String(20), unique=True, nullable=False)
