3a674e2711f67ef6c541a0fe512e883c2bbcbdb7
sein0211 <sein4037@gmail.com>
2021-08-16 21:34:30 +0900

    테이블 수정

--- pybo/models.py
+++ pybo/models.py
@@ -14,18 +14,18 @@ class TimeTable(db.Model):
 
 class DrowsinessData(db.Model):
     __table_name__='DrowsinessData'
-    d_data=db.Column(db.Integer,primary_key=True, nullable=False)
-    user_id = db.Column(db.Integer, db.ForeignKey('user.user_id', ondelete='CASCADE'))
-    user = db.relationship('User', backref=db.backref('d_data_set'))
-    d_time=db.Column(db.DateTime,nullable=False)
-    d_picture=db.Column(db.String(100),default='Unknown.jpg')
-    start_time=db.Column(db.DateTime,nullable=False)
-    end_time=db.Column(db.DateTime,nullable=False)
+    id = db.Column(db.Integer,primary_key=True)
+    user_id = db.Column(db.Integer, db.ForeignKey('user.id', ondelete='CASCADE'))
+    user = db.relationship('User', backref=db.backref('drowsiness_data_set'))
+    d_time = db.Column(db.DateTime,nullable=False)
+    d_picture = db.Column(db.String(100),default='Unknown.jpg')
+    start_time = db.Column(db.DateTime,nullable=False)
+    end_time = db.Column(db.DateTime,nullable=False)
 
 class User(db.Model):
     __table_name__='User'
-    user_id = db.Column(db.String(6), primary_key=True, nullable=False)
+    id = db.Column(db.Integer, primary_key=True)
     nickname = db.Column(db.String(20), nullable=False)
-    id = db.Column(db.String(20), unique=True, nullable=False)
+    student_id = db.Column(db.String(20), unique=True, nullable=False)
     password = db.Column(db.String(20), nullable=False)
     profile_image = db.Column(db.String(100), default='profile.png')
