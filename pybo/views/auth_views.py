2bec47b2136cd3fa6b9b81110f32e88828e58139
gorapaduckoo <67768976+gorapaduckoo@users.noreply.github.com>
2021-08-23 11:29:02 +0900

    Add files via upload

---
+++ pybo/views/auth_views.py
@@ -0,0 +1,26 @@
+# -*- coding: utf-8 -*-
+from flask import Blueprint, url_for, render_template, flash, request
+from werkzeug.security import generate_password_hash
+from werkzeug.utils import redirect
+
+from pybo import db
+from pybo.forms import UserCreateForm
+from pybo.models import User
+
+bp = Blueprint('auth', __name__, url_prefix='/auth')
+#auth라는 URL 접두어로 시작하는 URL이 호출되면 auth_views.py 파일의 함수들이 호출될 수 있도록 블루프린트 추
+
+@bp.route('/signup/', methods=('GET', 'POST'))
+def signup():
+    form = UserCreateForm()
+    if request.method == 'POST' and form.validate_on_submit():
+        user = User.query.filter_by(username=form.username.data).first()
+        if not user:
+            user = User(username=form.username.data,
+                        password=generate_password_hash(form.password1.data))
+            db.session.add(user)
+            db.session.commit()
+            return redirect(url_for('main.index'))
+        else:
+            flash('이미 존재하는 사용자입니다.')
+    return render_template('auth/signup.html', form=form)
