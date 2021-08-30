4596b75731d92cc54c7e674c2a1823ff0c8bbe5e
{bobsini601} <{psh7130@naver.com}>
2021-08-30 11:26:37 +0900

    - main 페이지 연결 _ '/' 로 주소설정 - 'CLASS 버튼'과 camera.html 버튼 연결 _ 누르면 카메라 실행 - 'main 페이지'에 student_id 출력

--- pybo/views/main_views.py
+++ pybo/views/main_views.py
@@ -1,14 +1,17 @@
-from flask import Blueprint
-from flask import render_template
-import cv2
+from flask import Blueprint, render_template, session,redirect
 
-bp = Blueprint('main', __name__, url_prefix='/')
-
+bp = Blueprint('main', __name__, template_folder='templates', url_prefix='/')
+# 여기서 main 은 url_for 함수에 사용된다.
 
 @bp.route('/')
 def index():
     return render_template('main.html')
     # 나중에 회원의 '닉네임'값을 넘겨줘야함. 그래야 출력해줄 수 있음
+
+@bp.route('/logout',methods=['GET'])
+def logout():
+    session.pop('student_id',None)
+    return redirect('/login')
 
 
 
